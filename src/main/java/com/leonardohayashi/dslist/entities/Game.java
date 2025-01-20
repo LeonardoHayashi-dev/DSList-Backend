14ad96d1b083044a24bfea87cbbbd780fbab9ff3
Leonardo Hayashi <leonardodearaujohay@gmail.com>
2025-01-20 12:53:25 -0300

    Game seed

--- src/main/java/com/leonardohayashi/dslist/entities/Game.java
+++ src/main/java/com/leonardohayashi/dslist/entities/Game.java
@@ -2,6 +2,8 @@ package com.leonardohayashi.dslist.entities;
 
 import java.util.Objects;
 
+import com.leonardohayashi.dslist.entities.Game;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,32 +16,37 @@ import jakarta.persistence.Table;
 public class Game {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private Double platforms;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game( ) {
-		
-	}
-
-	public Game(Long id, String title, Integer year, String genre, Double platforms, String imgUrl,
+    public Game() {
+    }
+    
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
 		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
 
 	public Long getId() {
@@ -74,12 +81,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public Double getPlatforms() {
+	public String getPlatforms() {
 		return platforms;
 	}
 
-	public void setPlatforms(Double platforms) {
+	public void setPlatforms(String platforms) {
 		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -122,7 +137,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
