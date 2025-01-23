7d7aaa08a0cbc13392f8e0cef463b42851e2c39e
Leonardo Hayashi <leonardodearaujohay@gmail.com>
2025-01-23 10:46:23 -0300

    GET game by id

--- src/main/java/com/leonardohayashi/dslist/services/GameService.java
+++ src/main/java/com/leonardohayashi/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.leonardohayashi.dslist.dto.GameDTO;
 import com.leonardohayashi.dslist.dto.GameMinDTO;
 import com.leonardohayashi.dslist.entities.Game;
 import com.leonardohayashi.dslist.repositories.GameRepository;
@@ -15,6 +17,11 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional (readOnly = true)
+	public GameDTO findById (Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO (result);
+	}
 	
 	public List<GameMinDTO> findAll () {
 		List<Game> result = gameRepository.findAll();
