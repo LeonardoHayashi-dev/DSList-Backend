11c03976234fbb7fd76414a1aedbb86c6b6234e0
Leonardo Hayashi <leonardodearaujohay@gmail.com>
2025-01-24 14:04:36 -0300

    Move games on list

--- src/main/java/com/leonardohayashi/dslist/services/GameListService.java
+++ src/main/java/com/leonardohayashi/dslist/services/GameListService.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.leonardohayashi.dslist.dto.GameListDTO;
 import com.leonardohayashi.dslist.entities.GameList;
+import com.leonardohayashi.dslist.projections.GameMinProjection;
 import com.leonardohayashi.dslist.repositories.GameListRepository;
+import com.leonardohayashi.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -15,6 +17,9 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+
 	
 	public List<GameListDTO> findAll () {
 		List<GameList> result = gameListRepository.findAll();
@@ -22,4 +27,18 @@ public class GameListService {
 		
 	}
 	
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(destinationIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		
+		for (int i = min; i<= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 }
